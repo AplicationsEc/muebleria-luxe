d194afe2e6bf85f08a57b7cf2ccfae8652a83131
Tomas Felipe <97459312+felipeAlmEspa@users.noreply.github.com>
2025-03-08 18:52:00 -0500

    Refactor project name from TestDrawerUno to PrimeApp and update related configurations

--- main/Nav.tsx
+++ main/Nav.tsx
@@ -1,7 +1,7 @@
 import { createDrawerNavigator } from "@react-navigation/drawer";
 import { HomeScreen } from "../screens/home/HomeScreen";
 import { SettingsScreen } from "../screens/settings/SettingsScreen";
-import { AZUL, GRIS, GRIS_OSCURO, NEGRO } from "../constants/Colors";
+import { AZUL, GRIS, GRIS_OSCURO, NEGRO, ROJO } from "../constants/Colors";
 import { NavigationContainer } from "@react-navigation/native";
 export const Main = () => {
   return (
@@ -14,12 +14,17 @@ export const Main = () => {
           headerStyle: {
             backgroundColor: NEGRO,
             opacity: 0.9,
-            maxHeight: 70,
           },
           headerTintColor: "#fff",
         }}
       >
-        <Drawer.Screen name="Home" component={HomeScreen} />
+        <Drawer.Screen
+          options={{
+            drawerContentStyle: { marginTop: -30 },
+          }}
+          name="Home"
+          component={HomeScreen}
+        />
         <Drawer.Screen name="Profile" component={SettingsScreen} />
       </Drawer.Navigator>
     </NavigationContainer>
--- screens/home/HomeScreen.tsx
+++ screens/home/HomeScreen.tsx
@@ -1,10 +1,21 @@
-import { Text, View } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
 import { GRIS_CLARO, GRIS_CLARO_2, GRIS_OSCURO } from "../../constants/Colors";
 
 export const HomeScreen = () => {
   return (
-    <View style={{ flex: 1, backgroundColor: GRIS_CLARO_2 }}>
+    <View style={styles.container}>
       <Text>TestXA</Text>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: GRIS_CLARO_2,
+  },
+  text: {
+    color: GRIS_OSCURO,
+    fontSize: 24,
+  },
+});
