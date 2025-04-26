402ea6315bf7c32f0afa64c5b937b8fa94c0d5b6
Tomas Felipe <97459312+felipeAlmEspa@users.noreply.github.com>
2025-04-26 16:15:59 -0500

    Refactor layout and SiteHeader components, update logo configuration, and remove send-email API: Enhance layout with dynamic logo integration, update SiteHeader to display the new logo, and delete the unused send-email API route.

--- components/SiteHeader.tsx
+++ components/SiteHeader.tsx
@@ -5,13 +5,20 @@ import { Button } from "@/components/ui/button";
 import { ThemeToggle } from "./ThemeToggle";
 import { configApp } from "@/helper/constants";
 import { useShoppingCart } from "./ShoppingCartProvider";
+import Image from "next/image";
 
 export function SiteHeader() {
   const { openCart } = useShoppingCart();
   return (
     <header className="sticky pl-5 pr-5 top-0 z-50 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
       <div className="container flex h-16 items-center justify-between">
-        <Link href="/" className="flex items-center space-x-2">
+        <Link href="#" className="flex items-center space-x-2">
+          <Image
+            src={configApp.logo + ".png"}
+            alt="Logo"
+            width={32}
+            height={32}
+          />
           <span className="text-xl font-bold">{configApp.nombreEmpresa}</span>
         </Link>
 
